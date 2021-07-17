48c220a89c8a584811e1a64448f3a6775c8d90b1
Archangel Macsika <drmacsika@gmail.com>
2021-07-17 08:46:00 +0100

    Add the next parameter to track previos url as a POST request.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from allauth.account.utils import get_next_redirect_url
+from allauth.account.views import EmailVerificationSentView, SignupView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class CustomSignupView(SignupView):
+    success_url = "reverse_lazy('accounts:email_verification_sent')"
+
+    def get_success_url(self):
+        ret = get_next_redirect_url(
+            self.request, self.redirect_field_name) or self.success_url
+        return ret
+
+
+class CustomEmailVerificationSentView(EmailVerificationSentView):
+    template_name = "accounts/verification_sent.html"
