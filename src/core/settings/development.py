df2c3cef15d934ec7cbca6dbd59791fc94984945
Archangel Macsika <drmacsika@gmail.com>
2021-07-25 14:17:44 +0100

    Added django debug package and two more settings file

--- src/core/settings/development.py
+++ src/core/settings/development.py
@@ -18,11 +18,20 @@ def get_secret(setting, secrets=secrets):
 
 
 SECRET_KEY = get_secret('SECRET_KEY')
-print(SECRET_KEY)
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 DATABASES = {
     'default': {
